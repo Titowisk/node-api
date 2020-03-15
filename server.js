4a308e7ace3b5741cad4f60b4479ae9eb8568f65
Vitor <rabelo51@gmail.com>
2020-03-14 22:11:46 -0300

    Nodejs Api RocketSeat

---
+++ server.js
@@ -0,0 +1,23 @@
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Inicia a aplicação 
+const app = express();
+app.use(express.json()); // permite à aplicação aceitar body em formato JSON
+
+// Inicia o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+requireDir('./src/models');
+
+/** Rotas
+ * Toda rota que começar por /api, será gerenciada pelo path fornecido
+ */
+app.use('/api', require('./src/routes'));
+app.use(cors());
+
+app.listen(3001);
